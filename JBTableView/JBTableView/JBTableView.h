eb3ffc2a205e1728cb5f3d468c18722ebbf53b82
jb32 <2062964913@qq.com>
2015-06-10 21:48:22 +0800

    初始化

---
+++ JBTableView/JBTableView/JBTableView.h
@@ -0,0 +1,74 @@
+//
+//  JBTableVIew.h
+//  JBTableView
+//
+//  Created by 薛靖博 on 15/5/17.
+//  Copyright (c) 2015年 xjb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class JBTableView;
+///刷新加载代理
+@protocol JBTableViewDelegate <NSObject>
+
+@optional
+/**
+ *下拉刷新 开始时调用
+ *@param tableView
+ */
+- (void)tableViewDidStartRefreshing:(JBTableView *)tableView;
+/**
+ *上拉加载 开始时调用
+ *@param tableView
+ */
+- (void)tableViewDidStartLoading:(JBTableView *)tableView;
+
+@end
+
+@protocol JBTableViewTouchDelegate <NSObject>
+@optional
+/**
+ *table view began touch
+ */
+- (void)tableView:(JBTableView *)tableView touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event;
+/**
+ *table view touch moved
+ */
+- (void)tableView:(JBTableView *)tableView touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event;
+/**
+ *table view touch end
+ */
+- (void)tableView:(JBTableView *)tableView touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event;
+
+@end
+///表格刷新状态
+typedef enum : NSUInteger {
+    JBStateNormal = 0,
+    JBStateStay,
+    JBStatePulling,
+    JBStateLoading,
+} KTableState;
+
+@interface JBTableView : UITableView {
+    BOOL  _isHaveRefreshView;
+    BOOL  _isHaveLoadView;
+}
+///上拉，下拉 刷新 代理
+@property (weak, nonatomic    ) IBOutlet id<JBTableViewDelegate          > JBDelegate;
+///点击tableview 时调用的 代理
+@property (weak, nonatomic    ) IBOutlet id<JBTableViewTouchDelegate> JBTouchDelegate;
+///NO 没有下拉刷新 默认YES
+@property (assign, nonatomic  ) BOOL                     isHaveRefreshView;
+///NO 没有上拉加载 默认YES
+@property (assign, nonatomic  ) BOOL                     isHaveLoadView;
+///NO 正在刷新、加载 YES 刷新、加载完成
+@property (readonly, nonatomic) BOOL                     isFinish;
+///表格刷新状态。默认为JBStateNormal
+@property (readonly, nonatomic) KTableState              state;
+/**
+ *刷新、加载结束
+ **/
+- (void)end;
+
+@end
